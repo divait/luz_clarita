01a89bc8f11fbe3d2430ebdc049fa4d69cd8582d
Cometware Soporte <cometware@artesguerrero.local>
2018-07-07 15:07:54 -0500

    Go

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+       /* public float speed;
+    public GameObject body;
+    
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+            MovementCharacter();
+        Debug.Log("h: " + Input.GetAxis("Horizontal") + "v: " + Input.GetAxis("Vertical"));
+
+        }
+
+        void MovementCharacter()
+        {
+            float moveH = Input.GetAxis("Horizontal");
+            float moveV = Input.GetAxis("Vertical");
+
+        transform.Translate((moveV * -speed) * Time.deltaTime, 0, (moveH * speed) * Time.deltaTime)  ;
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            body.transform.rotation = new Quaternion(0, 180, 0, 0);
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            body.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            body.transform.rotation = new Quaternion(0, 90, 0, 0);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            body.transform.rotation = new Quaternion(0, 270, 0, 0);
+        }
+
+    }*/
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(CapsuleCollider))]
+	[RequireComponent(typeof(Animator))]
+	public class ThirdPersonCharacter : MonoBehaviour
+	{
+		[SerializeField] float m_MovingTurnSpeed = 360;
+		[SerializeField] float m_StationaryTurnSpeed = 180;
+		[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
+		[SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
+		[SerializeField] float m_MoveSpeedMultiplier = 1f;
+		[SerializeField] float m_AnimSpeedMultiplier = 1f;
+
+		Rigidbody m_Rigidbody;
+		Animator m_Animator;
+		bool m_IsGrounded;
+		float m_OrigGroundCheckDistance;
+		const float k_Half = 0.5f;
+		float m_TurnAmount;
+		float m_ForwardAmount;
+		Vector3 m_GroundNormal;
+		float m_CapsuleHeight;
+		Vector3 m_CapsuleCenter;
+		CapsuleCollider m_Capsule;
+		bool m_Crouching;
+
+
+		void Start()
+		{
+			m_Animator = GetComponent<Animator>();
+			m_Rigidbody = GetComponent<Rigidbody>();
+			m_Capsule = GetComponent<CapsuleCollider>();
+			m_CapsuleHeight = m_Capsule.height;
+			m_CapsuleCenter = m_Capsule.center;
+
+			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+		}
+
+
+		public void Move(Vector3 move)
+		{
+
+			// convert the world relative moveInput vector into a local-relative
+			// turn amount and forward amount required to head in the desired
+			// direction.
+			if (move.magnitude > 1f) move.Normalize();
+			move = transform.InverseTransformDirection(move);
+			move = Vector3.ProjectOnPlane(move, m_GroundNormal);
+			m_TurnAmount = Mathf.Atan2(move.x, move.z);
+			m_ForwardAmount = move.z;
+
+			ApplyExtraTurnRotation();
+		}
+
+
+
+		void HandleAirborneMovement()
+		{
+			// apply extra gravity from multiplier:
+			Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
+			m_Rigidbody.AddForce(extraGravityForce);
+
+		}
+
+		void ApplyExtraTurnRotation()
+		{
+			// help the character turn faster (this is in addition to root rotation in the animation)
+			float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+			transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+		}
+
+        void FixedUpdate()
+        {
+            Move(transform.position);
+        }
+    }
+}
