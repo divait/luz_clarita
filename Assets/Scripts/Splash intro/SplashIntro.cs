6f5f8c28cb913068eb5947af37abed718eb6ffdd
GodZilla1212 <stivecreative@gmail.com>
2018-07-08 14:45:54 -0500

    escenas de carga y video

---
+++ Assets/Scripts/Splash intro/SplashIntro.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashIntro : MonoBehaviour {
+
+    public float time = 1.5f;
+    private AudioSource buttonClick;
+
+	void Start () {
+
+       buttonClick = GetComponent<AudioSource>();
+	}
+	
+	
+	void Update () {
+
+        if (Input.anyKeyDown)
+        {
+            buttonClick.Play();
+            Invoke("LoadVideoScene", time);
+        }
+    }
+    void LoadVideoScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    }
---
+++ Assets/Scripts/Splash intro/VideoIntro.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoIntro : MonoBehaviour {
+
+    private VideoPlayer vp;
+
+	void Start()
+    {
+
+        vp = GetComponent<VideoPlayer>();
+    }
+
+
+    void Update()
+    {
+
+        Invoke("Loadvideo", 56);
+
+    }
+
+    void Loadvideo()
+    {
+        if (vp.isPlaying == false)
+        {
+            SceneManager.LoadScene(2);
+
+        }
+        else
+        {
+
+            return;
+        }
+    }
+}
