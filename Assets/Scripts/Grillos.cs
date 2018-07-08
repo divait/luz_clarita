a7e4b4bcdedc2d86793333c657e3eb8b818d26ca
ARTESGUERRERO\cometware <divait_17@hotmail.com>
2018-07-08 13:13:54 -0500

    la vida de stiven

---
+++ Assets/Scripts/Grillos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grillos : MonoBehaviour {
+
+    private AudioSource grillos;
+    private float timeWait;
+
+	void Start ()
+    {
+        grillos = GetComponent<AudioSource>();
+		
+	}
+	
+	
+	void Update () {
+
+        if (grillos.isPlaying)
+        {
+            return;
+        }
+        else
+        {
+            StartCoroutine(TimeOfPlayGrillos());
+            
+        }
+		
+	}
+
+    IEnumerator TimeOfPlayGrillos()
+    {
+        yield return new WaitForSeconds(timeWait);
+        timeWait = Random.Range(4, 8);
+        grillos.Play();
+        
+        
+    }
+}
---
+++ Assets/Scripts/PingPongTexture.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongTexture : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public Color init;
+    public Color end;
+
+    public bool repeatable = false;
+    private float startTime;
+
+	void Start ()
+    {
+       
+        startTime = Time.time;
+	}
+
+
+    void Update()
+    {
+        if (!repeatable)
+        {
+            float t = (Time.time - startTime) * speed;
+            GetComponent<Renderer>().material.SetColor("_TintColor", Color.Lerp(init, end, t));
+        }
+        else
+        {
+            float t = (Mathf.Sin(Time.time - startTime) * speed);
+            GetComponent<Renderer>().material.SetColor("_TintColor", Color.Lerp(init, end, t));
+
+        }
+	}
+}
